07b39fa54123a03b3b2f57fc122f249edb707aa1
Mihai <mihai_visovan@yahoo.com>
2022-03-13 00:57:43 +0200

    Removed fontawesome config file

--- src/assets/fontAwesomeIcons.js
+++
@@ -1,8 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-
-const setIcons = () => {
-  library.add(faShoppingCart);
-};
-
-export default setIcons;
