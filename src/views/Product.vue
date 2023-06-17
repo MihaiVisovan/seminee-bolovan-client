90d7c2995bff18e873551dc2ecbba7fe67c021f9
MihaiVisovan <38044309+MihaiVisovan@users.noreply.github.com>
2023-06-17 15:40:37 +0300

    Added sections and dividers on product page

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,15 +1,32 @@
 <template>
-  <div :class="containerClass">{{ product.title }}</div>
+  <div class="product">
+    <div class="product__section product__section__one">
+      <div class="product__divider product__divider__gallery">
+        <Gallery />
+      </div>
+      <div class="product__divider product__divider__product_info">
+        <ProductInfo />
+      </div>
+    </div>
+    <div class="product__section product__section__two">
+      <ProductBenefits />
+    </div>
+    <div class="product__section product__section__three"></div>
+  </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
+import Gallery from '@/components/product/Gallery.vue';
+import ProductInfo from '@/components/product/ProductInfo.vue';
+import ProductBenefits from '@/components/product/ProductBenefits.vue';
+
 export default {
   name: 'Product',
-  data() {
-    return {
-      containerClass: 'product',
-    };
+  components: {
+    Gallery,
+    ProductInfo,
+    ProductBenefits,
   },
   created() {
     const productId = this.$route.params.productId;
@@ -26,12 +43,36 @@ export default {
 
 <style lang="scss" scoped>
 .product {
-  font-size: $font-5xl;
-  font-weight: bold;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
+  flex-direction: column;
+  padding: 0;
+  box-shadow: -2px -4px 10px -3px grey;
+
+  &__section {
+    display: flex;
+    &__one {
+      height: 700px;
+    }
+
+    &__two {
+      height: 120px;
+    }
+
+    &__three {
+    }
+  }
+
+  &__divider {
+    width: 50%;
+    padding: 50px 40px 0px 40px;
+
+    &__gallery {
+      background-color: #f2f2f2;
+    }
+
+    &__product_info {
+      box-shadow: -5px 0px 8px -7px grey;
+    }
+  }
 }
 </style>
