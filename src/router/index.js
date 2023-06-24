98e481bf8452f4161eb57278adba311413a4f314
MihaiVisovan <38044309+MihaiVisovan@users.noreply.github.com>
2023-06-24 20:01:54 +0300

    Added Search vue into views router

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const Categories = () => import('@/views/Categories.vue');
 const Products = () => import('@/views/Products.vue');
+const SearchComponent = () => import('@/views/SearchComponent.vue');
 const Product = () => import('@/views/Product.vue');
 const Cart = () => import('@/views/Cart.vue');
 
 const routes = [
   { path: '/', component: Categories, name: 'Categories' },
   { path: '/:categoryId', component: Products, name: 'Products' },
+  { path: '/search', component: SearchComponent, name: 'SearchComponent' },
   { path: '/:categoryId/:productId', component: Product, name: 'Product' },
   { path: '/cart', component: Cart, name: 'Cart' },
 ];
